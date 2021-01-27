5c32171834651c1ca0c51053c2811daccf862cb4
iron <472263356@qq.com>
2021-01-27 16:48:41 +0000

    update qiniu upload func

--- app/qiniu/index.py
+++ app/qiniu/index.py
@@ -7,14 +7,16 @@ def save_file_2_qiniu(path, filename):
         if QINIU_ACCESS_KEY and QINIU_SECRET_KEY:
             q = Auth(QINIU_ACCESS_KEY, QINIU_SECRET_KEY)
             token = q.upload_token(QINIU_BUCKET_NAME, filename, 3600)
-            print('start upload...')
+            print(f'{filename} start upload...')
             put_file(token, filename, path)
-            print('end upload!')
+            print(f'{filename} end upload!')
+        else:
+            print('No Key!')
     except Exception as e:
         raise Exception(f"Qiniu Err: {e}")
 
 
 if __name__ == "__main__":
-    url = input("please input user name: ")
+    path = input("please input file path: ")
     filename = input("please input filename: ")
-    save_file_2_qiniu(url, filename)
+    save_file_2_qiniu(path, filename)
